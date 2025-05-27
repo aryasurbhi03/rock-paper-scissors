52870bf83c6a7cfe70ee2ea0697f23f9d07eb60c
arya.surbhi <arya.surbhi03@gmail.com>
2025-05-27 13:46:11 -0400

    page object page

---
+++ test/pageobjects/home.page.js
@@ -0,0 +1,19 @@
+import { $ } from '@wdio/globals';
+
+/**
+ * 
+ */
+class HomePage {
+    /**
+     * define selectors using getter methods
+     */
+    get title() {
+        return $('//h1[contains(text(),"Rock Paper Scissors")]');
+    }
+
+    async open () {
+        await browser.url('/'); // Replace with the actual URL of your login page
+    }
+}
+
+export default new LoginPage();

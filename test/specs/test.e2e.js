b9a7f47f8c99590a9d9e9830cdcd8da6edece0cc
arya.surbhi <arya.surbhi03@gmail.com>
2025-05-27 15:00:49 -0400

    pushing changes to check ADO trigger

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -3,7 +3,7 @@ import HomePage from '../pageobjects/home.page'
 
 
 describe('My Rock Paper Scissors application', () => {
-    it('should load Rock Paper Scissors Game ', async () => {
+    it('should load Rock Paper Scissors Home Page ', async () => {
         await HomePage.open();
         await expect(HomePage.title).toBeDisplayed();
         await expect(HomePage.title).toHaveTextContaining('Rock Paper Scissors');
